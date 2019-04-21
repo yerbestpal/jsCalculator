bf0322aaa12da2d8d8018dcadb289a28650d0244
Ross McLean <rmclean92@posteo.net>
2019-04-21 18:01:30 +0100

    Impletmented (very) basic arithmetic. Fixed bugs.

--- functions.js
+++ functions.js
@@ -1,6 +1,3 @@
-// TODO: If display is 0, and result is 0, overright display zero with next number input
-//        or otherwise remove trailing zero on left of string.
-
 // TODO: if sum-string-display is unpopulated, do not populate it (i.e. when entering first number)
 
 // TODO: do not allow multiples of one real-world operation at a time (e.g. ++, --, /+, *+, etc.)
@@ -9,19 +6,104 @@
 
 
 // global variables
-// var displayValue = 0.0;
-// var currentValue = 0.0;
+var numString = ""; // used to build string to be converted to float and stored in heldValue
 var heldValue = 0.0;
 var result = 0.0;
-var operator = "";
-var operatorCheck = false;
+var operatorCheck = false; // checks if operator has been pressed.
 
 
-document.addEventListener("keydown", function (event){
+// updates variable and screen values to value of number button pressed
+function returnNumber(num) {
+
+    if (document.getElementById('screen').value.length <= 9) {
+
+        var thisNum = num;
+        if (numString == "") document.getElementById('screen').value = null;
+        document.getElementById('screen').value += num;
+        document.getElementById('screen').style.textAlign = "right";
+        document.getElementById('sum-string-display').innerHTML += thisNum;
+        numString += thisNum; 
+        operatorCheck = false;
+        console.log("numString: " + numString);
+
+    } else {
+
+        console.log("There is a number limit.");
+
+    }
+
+};
+
+// used by math operator buttons to set the operator variable
+function setOperator (op) {
+
+    operatorCheck = true;
+    document.getElementById('screen').value = "";
+    document.getElementById('sum-string-display').innerHTML += op;
+    console.log(op);
+
+    switch (op) {
+
+        case "+":
+            heldValue += Number(numString);  
+            numString = "";
+            break;
+        case "-":
+            heldValue -= Number(numString);  
+            numString = ""; 
+            break;
+        case "*":
+            heldValue *= Number(numString);  
+            numString = ""; 
+            break;
+        case "/":
+            heldValue /= Number(numString);  
+            numString = "";
+            break;
+
+    }
+
+}
+
+// wipe set variable and screen values to 0
+function clearAll() {
+
+    numString = "";
+    heldValue = 0.0;
+    result = 0.0;
+    operatorCheck = false;
+    document.getElementById('screen').style.textAlign = "right";
+    document.getElementById('screen').value = heldValue;
+    document.getElementById('sum-string-display').innerHTML = "";
+    console.log("Clear All");
+}
+
+// calculates final answer
+function equals() {
+
+    if (numString != "" || numString > 0) {
+
+        heldValue += Number(numString);
+        numString = "";
+
+    }
+
+    result = heldValue;
+    operatorCheck = false;
+    document.getElementById('screen').value = result;
+    document.getElementById('sum-string-display').innerHTML = "";
+    document.getElementById('screen').style.textAlign = "left";
+    console.log("result: " + result);
+
+}
+
+document.addEventListener("keydown", function (event) {
 
     switch (event.key) {
         case  "1":
             returnNumber(1);
+            // var numAudio = new Audio("assets/audio/number.wav");
+            numAudio.play();
             break;
         case "2":
             returnNumber(2);
@@ -73,91 +155,4 @@ document.addEventListener("keydown", function (event){
             break;
     }   
 
-})
-
-
-// updates variable and screen values to value of number button pressed
-function returnNumber(num) {
-
-    if (document.getElementById('screen').value.length <= 9) {
-
-        var thisNum = num;
-        document.getElementById('screen').style.textAlign = "right";
-        if (heldValue == 0) document.getElementById('screen').value = "";
-        document.getElementById('screen').value += num;
-
-        if (operatorCheck == true) {
-
-            switch (operator) {
-
-                case "+":
-                    document.getElementById('sum-string-display').innerHTML += thisNum;
-                    heldValue += num;
-                    operatorCheck = false; 
-                    break;
-                case "-":
-                    document.getElementById('sum-string-display').innerHTML += thisNum;
-                    heldValue -= num;
-                    operatorCheck = false; 
-                    break;
-                case "*":
-                    document.getElementById('sum-string-display').innerHTML += thisNum;
-                    heldValue *= num;
-                    operatorCheck = false; 
-                    break;
-                case "/":
-                    document.getElementById('sum-string-display').innerHTML += thisNum;
-                    heldValue /= num;
-                    operatorCheck = false; 
-                    break;
-        
-            }
-
-        } else {
-
-            document.getElementById('sum-string-display').innerHTML += thisNum.toString();
-            heldValue += num;
-            operatorCheck = false;
-
-        }
-
-    } else {
-
-        console.log("There is a number limit.");
-
-    }
-
-};
-
-// used by math operator buttons to set the operator variable
-function setOperator (op) {
-
-    operator = op;
-    operatorCheck = true;
-    document.getElementById('screen').value = "";
-    document.getElementById('sum-string-display').innerHTML += operator;
-
-}
-
-// wipe set variable and screen values to 0
-function clearAll() {
-
-    heldValue = 0.0;
-    result = 0.0;
-    operatorCheck = false;
-    document.getElementById('screen').style.textAlign = "right";
-    document.getElementById('screen').value = heldValue;
-    document.getElementById('sum-string-display').innerHTML = "";
-}
-
-// calculates final answer
-function equals() {
-
-    result = heldValue;
-    operatorCheck = false;
-    document.getElementById('screen').value = result;
-    document.getElementById('sum-string-display').innerHTML = "";
-    document.getElementById('screen').style.textAlign = "left";
-    Document.getElementById('result_test').innerHTML += result;
-
-}
+});
